a8a00b10b780b6fd2ef372a17f9ea6683cc19a41
Amanda Wixted <amanda@amandawixted.com>
2018-04-10 10:12:22 -0400

    tack on a basic blank macOS app

---
+++ CrochetCharts/CrochetCharts/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CrochetCharts
+//
+//  Created by Amanda Wixted on 4/10/18.
+//  Copyright © 2018 Amanda Wixted. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
